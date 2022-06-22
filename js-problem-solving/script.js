2c6ee76485243d59678f3f1e951516ef6434c1ec
Tanmay_Pillai <omtej4499@gmail.com>
2022-06-22 17:29:03 +0530

    upload

--- js-problem-solving/script.js
+++ js-problem-solving/script.js
@@ -332,3 +332,10 @@ function fiboTri (n) {
     console.log(ans)
 }
 
+
+let strArray = [ "q", "w", "w", "w", "e", "i", "u", "r"];
+let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+
+console.log(findDuplicates(strArray)) // All duplicates
+console.log([...new Set(findDuplicates(strArray))]) /
+
